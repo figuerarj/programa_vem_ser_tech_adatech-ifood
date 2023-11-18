0fb0116f09209823d897701090c97c8865e9916c
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-11-18 16:12:01 -0300

    criacao do js - desafio final modulo 2

---
+++ exercicios/aula_016/desafio_final_modulo2/index.js
@@ -0,0 +1,5 @@
+let tasks = [{id: 1700334667097 , title:"lavar roupa", description:"lavar a roupa da semana"}, {id: 1700334667097 , title:"tirar o lixo", description:"pegar o lixo da cozinha e levar pra fora."},]
+
+function createTask(){
+
+}
