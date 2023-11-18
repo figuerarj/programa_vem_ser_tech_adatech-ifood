ef416abb58f645be683ca577907dd4e80664cb9f
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-11-18 17:28:26 -0300

    create a task to create an object

--- exercicios/aula_016/desafio_final_modulo2/index.js
+++ exercicios/aula_016/desafio_final_modulo2/index.js
@@ -1,5 +1,32 @@
 let tasks = [{id: 1700334667097 , title:"lavar roupa", description:"lavar a roupa da semana"}, {id: 1700334667097 , title:"tirar o lixo", description:"pegar o lixo da cozinha e levar pra fora."},]
 
-function createTask(){
-
+function createTask(title, description){
+  const idNumber = new Date().getTime();
+  tasks.push(
+    {
+        id: idNumber, 
+        title: title,
+        description: description, 
+    })
 }
+
+function removeTask(title){
+    const taskToDelete =  tasks.find(item => item.title == title)
+    console.log(taskToDelete)
+    return tasks.map(item => {
+       if( item !== taskToDelete){
+
+       }})
+
+    
+}
+
+//TODO editar uma tarefa
+//TODO lista de tarefas
+//TODO obter uma tarefa
+
+console.log(tasks);
+console.log("----------------")
+removeTask("lavar roupa")
+console.log("----------------")
+console.log(tasks)
