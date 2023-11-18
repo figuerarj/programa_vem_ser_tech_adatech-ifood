f4ee390fb82677536da38f3017d45833ef7c65b8
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-11-18 18:37:41 -0300

    updt

--- exercicios/aula_016/desafio_final_modulo2/index.js
+++ exercicios/aula_016/desafio_final_modulo2/index.js
@@ -1,32 +1,52 @@
 let tasks = [{id: 1700334667097 , title:"lavar roupa", description:"lavar a roupa da semana"}, {id: 1700334667097 , title:"tirar o lixo", description:"pegar o lixo da cozinha e levar pra fora."},]
+let tasksDeleted = []
 
 function createTask(title, description){
   const idNumber = new Date().getTime();
-  tasks.push(
-    {
-        id: idNumber, 
-        title: title,
-        description: description, 
-    })
+
+  if(tasks.find(item => item.title == title) !== undefined)
+  {
+    console.log("Já existe essa tarefa! Crie uma outra ou edite a tarefa existente");
+  } 
+  else {
+      tasks.push(
+        {
+            id: idNumber, 
+            title: title,
+            description: description, 
+        }
+        )
+        console.log(`${tasks.find(item => item.title == title)} foi criado com sucesso!`)
+  }
 }
 
 function removeTask(title){
     const taskToDelete =  tasks.find(item => item.title == title)
-    console.log(taskToDelete)
-    return tasks.map(item => {
-       if( item !== taskToDelete){
-
-       }})
+    
+    if (taskToDelete){
+        tasksDeleted.push(taskToDelete)
+        const index = tasks.indexOf(taskToDelete)
+        tasks.splice(index,1)
+    }
 
     
+}
+
+function editTask(){
+
 }
 
 //TODO editar uma tarefa
 //TODO lista de tarefas
 //TODO obter uma tarefa
-
-console.log(tasks);
-console.log("----------------")
-removeTask("lavar roupa")
-console.log("----------------")
+createTask("lavar o carro", "teste teste teste")
 console.log(tasks)
+// console.log(tasks);
+// console.log("----------------")
+// console.log(tasksDeleted)
+// removeTask("lavar roupa")
+// console.log("----------------")
+// console.log("----------------")
+// console.log(tasks)
+// console.log("----------------")
+// console.log(tasksDeleted)
