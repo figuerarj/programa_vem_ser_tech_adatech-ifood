313cb776ccf7a9f852a3ff407ae0dd11cf449adf
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-12-06 14:19:13 -0300

    tasks done

---
+++ exercicios/aula_021/atividade3.js
@@ -0,0 +1,19 @@
+class WordsCounter{
+
+    constructor(sentence){
+        this.sentence= sentence;
+        //ao inves de deixar a variavel words dentro da função eu posso trazer ela pra o construtor utilizando do this e se precisar eu posso usar o this e trazer o valor para dentro de um novo metodo.
+        this.words = this.countWords();
+    }
+
+    countWords(){
+        //retorna um array com as palavras.
+        return this.sentence.split(/\s+/)
+    }
+}
+
+//instancia da classe WordsCounter
+const wordsCounter = new WordsCounter('JavaScript is a powerful programming language! BOOOM!!!')
+//pega o length de words que esta dentro da instancia WordsCounter.
+const totalWords = wordsCounter.words.length;
+console.log(`Total words in the sentence: ${totalWords}`)
---
+++ exercicios/aula_021/atividade4.js
@@ -0,0 +1,23 @@
+class Client{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+        this.accounts = [];
+    }
+}
+
+
+class Account{
+
+
+    withdraw(){
+
+    }
+
+
+
+}
+
+
+
+const client = new Client("Rodrigo", 42)
