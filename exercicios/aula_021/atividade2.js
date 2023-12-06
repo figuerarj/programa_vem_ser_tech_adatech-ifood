f934b234b74070614ad111a8ac5825513e67bb87
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-12-06 13:22:43 -0300

    finalizada a tarefa 2

--- exercicios/aula_021/atividade2.js
+++ exercicios/aula_021/atividade2.js
@@ -1,4 +1,20 @@
 class CurrencyConverter{
-    exchangeRate = 2;
     
+    constructor(exchangeRate){
+        this.exchangeRate = exchangeRate
+    }
+    
+    convert(value, sourceCurrency, targetCurrency){
+        const convertedValue = value * this.exchangeRate;
+        console.log(`${value} ${sourceCurrency} is equivalent to ${convertedValue} ${targetCurrency}`);
+        return convertedValue;
+    }
 }
+
+// Criando instância do conversor de moeda
+const currencyConverterInstance = new CurrencyConverter(5.0);
+
+// Convertendo moeda atraves do metodo convert que faz parte da classe CurrencyConverter
+const targetCurrency = 'BRL'
+const convertedValue = currencyConverterInstance.convert(100, 'USD', targetCurrency);
+console.log(`Converted value: ${convertedValue} ${targetCurrency}`);
