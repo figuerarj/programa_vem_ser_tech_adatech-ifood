6c313039af3002a9751726b5479fc9ca335ba05f
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2023-12-14 00:38:05 -0300

    update

--- exercicios/aula_021/atividade3.js
+++ exercicios/aula_021/atividade3.js
@@ -1,7 +1,7 @@
 class WordsCounter{
 
     constructor(sentence){
-        this.sentence= sentence;
+        this.sentence= sentence.trim();
         //ao inves de deixar a variavel words dentro da função eu posso trazer ela pra o construtor utilizando do this e se precisar eu posso usar o this e trazer o valor para dentro de um novo metodo.
         this.words = this.countWords();
     }
@@ -13,7 +13,7 @@ class WordsCounter{
 }
 
 //instancia da classe WordsCounter
-const wordsCounter = new WordsCounter('JavaScript is a powerful programming language! BOOOM!!!')
+const wordsCounter = new WordsCounter(' Javascript é muito legal e diveritido!!!! ')
 //pega o length de words que esta dentro da instancia WordsCounter.
 const totalWords = wordsCounter.words.length;
 console.log(`Total words in the sentence: ${totalWords}`)
