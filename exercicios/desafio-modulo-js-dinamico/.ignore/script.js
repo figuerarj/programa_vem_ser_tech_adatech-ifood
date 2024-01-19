77811ed6d555c2f577b1e512cbcbc6f0b262d8a2
Rodrigo Santos <79957513+figuerarj@users.noreply.github.com>
2024-01-19 15:51:00 -0300

    versao 1 - finalizada

--- exercicios/desafio-modulo-js-dinamico/.ignore/script.js
+++ exercicios/desafio-modulo-js-dinamico/.ignore/script.js
@@ -2,7 +2,6 @@ const key = "1562a8c142ef4944f71d2543961d6236";
 const form = document.querySelector("#form_busca");
 const inputCidade = document.querySelector("#input_cidade");
 
-const dados = document.querySelector("#dados");
 const nameCity = document.querySelector("#name_city");
 const temp = document.querySelector("#temp");
 const tempMin = document.querySelector("#temp_min");
@@ -10,7 +9,7 @@ const tempMax = document.querySelector("#temp_max");
 const humidity = document.querySelector("#humdity");
 const pressure = document.querySelector("#pressure");
 const descriptWeather = document.querySelector("#descript_weather");
-const descriptIcon = document.querySelector("#descript_icon");
+const descriptIcon = document.querySelector("#descript_icon img");
 const windDeg = document.querySelector("#wind_deg");
 const windSpeed = document.querySelector("#wind_speed");
 
@@ -22,20 +21,21 @@ form.addEventListener("submit", (event) => {
   )
     .then((response) => response.json())
     .then((json) => {
+      console.log(json);
       nameCity.innerHTML = json.name
-      temp.innerHTML = `${json.main.temp.toFixed(0)} °C`
-      tempMin.innerHTML = `${json.main.temp_min.toFixed(0)} °C`
-      tempMax.innerHTML = `${json.main.temp_max.toFixed(0)} °C`
-      humidity.innerHTML = `${json.main.humidity} %`
-      pressure.innerHTML = `${json.main.pressure} Pa`
+      temp.innerHTML = json.main.temp
+      tempMin.innerHTML = json.main.temp_min
+      tempMax.innerHTML = json.main.temp_max
+      humidity.innerHTML = json.main.humidity
+      pressure.innerHTML = json.main.pressure
       descriptWeather.innerHTML = json.weather[0].description
       descriptIcon.src = `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`
       windDeg.innerHTML = json.wind.deg
       windSpeed.innerHTML = json.wind.speed
-      dados.classList.remove('d-none');
+      
+      
     })
     .catch((error) => {
-      alert("Ocorreu um erro");
-      dados.classList.add('d-none');
+      alert("ocorreu um erro");
     });
 });
